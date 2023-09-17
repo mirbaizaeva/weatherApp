297d176b9d0db42d5fd730c425ebcc3a2eff590b
Nurjamal <n.mirbaizaeva@gmail.com>
2023-09-18 01:41:07 +0600

    Weather

---
+++ weatherApp/Views/WelcomeView.swift
@@ -0,0 +1,42 @@
+//
+//  WelcomeView.swift
+//  weatherApp
+//
+//  Created by Nurjamal Mirbaizaeva on 17/9/23.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the WeatherApp")
+                    .bold().font(.title)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
