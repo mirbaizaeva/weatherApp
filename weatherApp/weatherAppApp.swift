9792aa60c66e83f122bb651fe5b78a5df69e4bc3
Nurjamal <n.mirbaizaeva@gmail.com>
2023-09-17 18:22:59 +0600

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Nurjamal Mirbaizaeva on 17/9/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
